20f92d0d882c446151c6d5c3001851e3e32a5681
Vojan-Najov <serejah.mashichev@gmail.com>
2023-08-20 21:39:17 +0300

    Fix Dockerfile.

--- tests/run.sh
+++ tests/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-#cd ../
-docker build . -t cat_grep_test:1.0 -f tests/Dockerfile
-docker run cat_grep_test:1.0
+docker build . -t cat_test:1.0 -f tests/Dockerfile
+docker run cat_test:1.0
